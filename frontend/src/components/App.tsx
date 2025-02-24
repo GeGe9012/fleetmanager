11df76039c05b4b2417b3e39a6b970b9e03ebb9e
GeGe9012 <gergo.molnar90@gmail.com>
2025-02-24 16:19:55 +0100

    add style to th elements

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,40 +1,42 @@
-import ResizableTable from './ResizableTable';
+import ResizableTable from "./ResizableTable";
 export default function App() {
-    return (
-      <div className="App">
-        Resizable column table
-        <ResizableTable resizable={true} resizeOptions={{}}>
-          <thead>
-          <tr>
-            <th>#</th>
-            <th>Make</th>
-            <th>Modell</th>
-            <th>Color</th>
-            <th>Fuel</th>
-            <th>Registration</th>
-            <th>Drivetrain</th>
-            <th>Company</th>
-            <th>Contract</th>
+  const thStyle = {overflow: "hidden", whiteSpace: "nowrap"}
+  return (
+    <div>
+      <ResizableTable resizable={true} resizeOptions={{}}>
+        <thead>
+          <tr >
+            <th style={thStyle}>#</th>
+            <th style={thStyle}>Make</th>
+            <th style={thStyle}>Modell</th>
+            <th style={thStyle}>Color</th>
+            <th style={thStyle}>Fuel</th>
+            <th style={thStyle}>Registration</th>
+            <th style={thStyle}>Drivetrain</th>
+            <th style={thStyle}>Warranty</th>
+            <th style={thStyle}>Company</th>
+            <th style={thStyle}>Contract</th>
+            <th style={thStyle}>Contract duration</th>
           </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-            </tr>
-          </tbody>
-        </ResizableTable>
-      </div>
-    );
-  }
+        </thead>
+        <tbody style={{ padding: "2px", textAlign: "center" }}>
+          <tr>
+            <td>12222222222222eweweeeeeeeeee</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+        </tbody>
+      </ResizableTable>
+    </div>
+  );
+}
