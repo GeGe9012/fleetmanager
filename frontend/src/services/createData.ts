b41e3934cf73fc5b5982ffa15cf8f115c7714cd8
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-02 16:52:40 +0100

    create createData ts

---
+++ frontend/src/services/createData.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { BACKEND_URL } from "../constants/backend";
+
+export async function createData(queryParams = {}) {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/cars`, {
+      params: queryParams,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
