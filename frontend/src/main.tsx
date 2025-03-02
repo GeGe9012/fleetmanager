6934e427f0075b69977e85461b0a57b6bb4299d1
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-02 12:40:53 +0100

    init react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/App";
+import NewData from "./components/NewData";
+import NotFoundPage from "./components/NotFoundPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/newdata",
+    element: <NewData />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <div className="m-4">
-    <App />
+    <RouterProvider router={router} />
   </div>
 );
