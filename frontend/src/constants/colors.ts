427502f386b215d28cd658ce0853fc2605dbd812
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-04 11:51:32 +0100

    add colors.ts

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,24 @@
+const colors: string[] = [
+    "Black",
+    "White",
+    "Silver",
+    "Gray",
+    "Red",
+    "Blue",
+    "Green",
+    "Yellow",
+    "Orange",
+    "Brown",
+    "Purple",
+    "Gold",
+    "Burgundy",
+    "Turquoise",
+    "Navy Blue",
+    "Olive Green",
+    "Bronze",
+    "Matte Black",
+    "Pearl White",
+    "Chameleon",
+  ];
+  
+  export default colors;
