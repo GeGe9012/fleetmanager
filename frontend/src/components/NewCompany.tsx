1009bc087fe8bf22090a37087108a3e73dedb81b
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-05 13:45:29 +0100

    fixing company form

--- frontend/src/components/NewCompany.tsx
+++ frontend/src/components/NewCompany.tsx
@@ -1,14 +1,20 @@
 import { useFormik } from "formik";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
-import validationSchema from "../schema/newDataSchema";
+import validationSchema from "../schema/newCompanySchema";
 import { createData } from "../services/createData";
+import countries from "../constants/countries";
 
 export default function NewCompany() {
   const formik = useFormik({
     initialValues: {
-      name: "",
-      email: "",
-      password: "",
+      company: "",
+      company_tax_number: "",
+      phone_number: "",
+      reg_number: "",
+      zipcode: "",
+      country: "",
+      city: "",
+      street_address: "",
     },
     validationSchema,
     onSubmit: async (values, { resetForm }) => {
@@ -32,77 +38,147 @@ export default function NewCompany() {
     <Container className="mt-5">
       <Row className="justify-content-md-center">
         <Col md={6}>
-          <h2 className="mb-4">Regisztrációs Űrlap</h2>
+          <h2 className="mb-4">Add new company</h2>
           <Form noValidate onSubmit={formik.handleSubmit}>
-            {/* Név */}
-            <Form.Group controlId="name">
-              <Form.Label>Név</Form.Label>
+            <Form.Group controlId="company">
+              <Form.Label>Company</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Add meg a neved"
-                name="name"
-                value={formik.values.name}
+                name="company"
+                value={formik.values.company}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                isInvalid={formik.touched.name && !!formik.errors.name}
+                isInvalid={formik.touched.company && !!formik.errors.company}
               />
               <Form.Control.Feedback type="invalid">
-                {formik.errors.name}
+                {formik.errors.company}
               </Form.Control.Feedback>
             </Form.Group>
 
-            {/* Email */}
-            <Form.Group controlId="email" className="mt-3">
-              <Form.Label>Email cím</Form.Label>
+            <Form.Group controlId="companyTaxNumber" className="mt-3">
+              <Form.Label>Tax Number</Form.Label>
               <Form.Control
-                type="email"
-                placeholder="Email címed"
-                name="email"
-                value={formik.values.email}
+                type="text"
+                name="company_tax_number"
+                value={formik.values.company_tax_number}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                isInvalid={formik.touched.email && !!formik.errors.email}
+                isInvalid={
+                  formik.touched.company_tax_number &&
+                  !!formik.errors.company_tax_number
+                }
               />
               <Form.Control.Feedback type="invalid">
-                {formik.errors.email}
+                {formik.errors.company_tax_number}
               </Form.Control.Feedback>
             </Form.Group>
 
-            {/* Jelszó */}
-            <Form.Group controlId="password" className="mt-3">
-              <Form.Label>Jelszó</Form.Label>
+            <Form.Group controlId="phoneNumber" className="mt-3">
+              <Form.Label>Contact Phone Number</Form.Label>
               <Form.Control
-                type="password"
-                placeholder="Jelszavad"
-                name="password"
-                value={formik.values.password}
+                type="text"
+                name="phone_number"
+                value={formik.values.phone_number}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                isInvalid={formik.touched.password && !!formik.errors.password}
+                isInvalid={
+                  formik.touched.phone_number && !!formik.errors.phone_number
+                }
               />
               <Form.Control.Feedback type="invalid">
-                {formik.errors.password}
+                {formik.errors.phone_number}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="phoneNumber" className="mt-3">
+              <Form.Label>Registration Number</Form.Label>
+              <Form.Control
+                type="text"
+                name="reg_number"
+                value={formik.values.reg_number}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                isInvalid={
+                  formik.touched.reg_number && !!formik.errors.reg_number
+                }
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.reg_number}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <h4 className="mt-4">Company address</h4>
+
+            <Form.Group controlId="zipcode" className="mt-3">
+              <Form.Label>Zipcode</Form.Label>
+              <Form.Control
+                type="text"
+                name="zipcode"
+                value={formik.values.zipcode}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                isInvalid={formik.touched.zipcode && !!formik.errors.zipcode}
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.zipcode}
               </Form.Control.Feedback>
             </Form.Group>
 
-            {/* Feltételek elfogadása
-            <Form.Group controlId="terms" className="mt-3">
-              <Form.Check
-                type="checkbox"
-                label="Elfogadom a feltételeket"
-                name="terms"
-                checked={formik.values.terms}
+            <Form.Group controlId="country" className="mt-3">
+              <Form.Label>Country</Form.Label>
+              <Form.Select
+                as="select"
+                name="country"
+                value={formik.values.country}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                isInvalid={formik.touched.terms && !!formik.errors.terms}
-                feedback={formik.errors.terms}
-                feedbackType="invalid"
-              />
-            </Form.Group> */}
+                isInvalid={formik.touched.country && !!formik.errors.country}
+              >
+                <option value="">Select a country...</option>
+                {countries.map((country, index) => (
+                  <option key={index} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.country}
+              </Form.Control.Feedback>
+            </Form.Group>
 
-            {/* Küldés gomb */}
+            <Form.Group controlId="city" className="mt-3">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                name="city"
+                value={formik.values.city}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                isInvalid={formik.touched.city && !!formik.errors.city}
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.city}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group controlId="streetAddress" className="mt-3">
+              <Form.Label>Street & House Number</Form.Label>
+              <Form.Control
+                type="text"
+                name="street_address"
+                value={formik.values.street_address}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                isInvalid={
+                  formik.touched.street_address &&
+                  !!formik.errors.street_address
+                }
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.street_address}
+              </Form.Control.Feedback>
+            </Form.Group>
+
             <Button variant="primary" type="submit" className="mt-4 w-100">
-              Regisztráció
+              Add new company
             </Button>
           </Form>
         </Col>
