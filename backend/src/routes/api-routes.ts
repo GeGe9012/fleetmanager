2c67c1090d1381e047cb6415f0d747c7cb34cc26
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-16 11:31:29 +0100

    integrate customer-routes

--- backend/src/routes/api-routes.ts
+++ backend/src/routes/api-routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import carRoutes from "./car-routes";
+import customerRoutes from "./customer-routes"
 
 const router = express.Router();
 
 router.use("/cars", carRoutes);
+router.use("/customers", customerRoutes);
 
 export default router;
