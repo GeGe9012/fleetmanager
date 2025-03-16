e297836afb42dbe8789b3a1fc344e48a6d6d35ba
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-16 11:30:58 +0100

    create car-controller.ts

---
+++ backend/src/controllers/customer-controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import customerService from "../services/customer-service";
+
+const customerController = {
+    async getAllCustomers(req: Request, res: Response) {
+        try {
+            const customers = await customerService.getAllCustomers();
+            res.json(customers);
+        } catch (error) {
+            res.status(500).json({ error: "An error occurred while retrieving the data." });
+        }
+    }
+};
+
+export default customerController;
