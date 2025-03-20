59c54b06044dbbe461daa8e643bd2a9d17bdbe1e
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-20 17:08:45 +0100

    add create company feature

--- backend/src/services/company-service.ts
+++ backend/src/services/company-service.ts
@@ -2,6 +2,17 @@ import { HTTP_STATUS_CODES } from "../constants/http-status-codes";
 import HttpError from "../utils/http-error";
 import prisma from "../db/prisma";
 
+interface NewCompanyData {
+  company: string;
+  company_tax_number: string;
+  contact_phone_number: string;
+  reg_number: string;
+  company_address_1: string;
+  company_address_2: string;
+  company_address_3: string;
+  company_address_4: string;
+}
+
 const companyService = {
   async getAllCompanies(filters: Record<string, any> = {}) {
     try {
@@ -23,6 +34,21 @@ const companyService = {
     } catch (err) {
       throw new HttpError(
         "Something went wrong",
+        HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
+      );
+    }
+  },
+
+  async createCompany(NewCompanyData: NewCompanyData) {
+    try {
+      const company = await prisma.company.create({
+        data: NewCompanyData,
+      });
+      return company;
+    } catch (err: any) {
+      console.error("Prisma Error:", err);
+      throw new HttpError(
+        err.message || "Company could not be created.",
         HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
       );
     }
