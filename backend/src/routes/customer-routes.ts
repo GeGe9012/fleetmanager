a5bf8424bc25a3cdc1dbc5c9701beb8e57348e67
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-20 16:01:38 +0100

    add create customer route

--- backend/src/routes/customer-routes.ts
+++ backend/src/routes/customer-routes.ts
@@ -4,5 +4,6 @@ import customerController from "../controllers/customer-controller";
 const router = express.Router();
 
 router.get("/", customerController.getAllCustomers);
+router.post("/", customerController.createCustomer);
 
 export default router;
