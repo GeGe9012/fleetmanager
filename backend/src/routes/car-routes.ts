ec9b6c840bd5753b1fd967a547e6788ba4b261bc
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-20 15:02:59 +0100

    add createCar route

--- backend/src/routes/car-routes.ts
+++ backend/src/routes/car-routes.ts
@@ -4,5 +4,6 @@ import carController from "../controllers/car-controller";
 const router = express.Router();
 
 router.get("/", carController.getAllCars);
+router.post("/", carController.createCar);
 
 export default router;
