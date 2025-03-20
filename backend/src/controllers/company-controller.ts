c1bc59b0f8dc3e1ff93b07edfe58728dc0650614
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-20 17:08:48 +0100

    add create company feature

--- backend/src/controllers/company-controller.ts
+++ backend/src/controllers/company-controller.ts
@@ -13,6 +13,16 @@ const companyController = {
         .json({ error: "An error occurred while retrieving the data." });
     }
   },
+  async createCompany(req: Request, res: Response) {
+    try {
+      const newCompany = await companyService.createCompany(req.body);
+      res.status(HTTP_STATUS_CODES.CREATED).json(newCompany);
+    } catch (err) {
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ error: "An error occurred while creating data." });
+    }
+  },
 };
 
 export default companyController;
