f340373eca8105de2c79bd52dd23243879d6b1fe
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-20 15:04:41 +0100

    add company routes to api routes

--- backend/src/routes/api-routes.ts
+++ backend/src/routes/api-routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import carRoutes from "./car-routes";
 import customerRoutes from "./customer-routes"
+import companyRoutes from "./company-routes"
 
 const router = express.Router();
 
 router.use("/cars", carRoutes);
 router.use("/customers", customerRoutes);
+router.use("/companies", companyRoutes);
 
 export default router;
