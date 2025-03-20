0b41fc73de955d42a1d26e2baa1cf15ffeaa7a96
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-20 16:02:17 +0100

    integrate create customer in frontend customer service

--- frontend/src/services/customerService.ts
+++ frontend/src/services/customerService.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
 import { BACKEND_URL } from "../constants/backend";
 
+interface Customer {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  email: string;
+  customer_address_1: string;
+  customer_address_2: string;
+  customer_address_3: string;
+  customer_address_4: string;
+  customer_tax_number: string;
+}
+
 export async function getAllCustomers(queryParams = {}) {
   try {
     const response = await axios.get(`${BACKEND_URL}/api/customers`, { params: queryParams });
@@ -8,5 +20,20 @@ export async function getAllCustomers(queryParams = {}) {
   } catch (error) {
     console.error("An error occurred while fetching customers:", error);
     throw new Error("Failed to fetch customers. Please try again later!");
+  }
+}
+
+export async function createCustomer(customer: Customer) {
+  try {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/customers`,
+      {
+        ...customer,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("An error occurred while creating customer:", error);
+    throw new Error("Failed to create customer. Please try again later!");
   }
 }
