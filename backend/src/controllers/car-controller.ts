bcff1d5ddf17fbd363fd0351d8917dd1677a1ef3
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-20 15:02:41 +0100

    add createCar function

--- backend/src/controllers/car-controller.ts
+++ backend/src/controllers/car-controller.ts
@@ -1,15 +1,28 @@
 import { Request, Response } from "express";
 import carService from "../services/car-service";
+import { HTTP_STATUS_CODES } from "../constants/http-status-codes";
 
 const carController = {
-    async getAllCars(req: Request, res: Response) {
-        try {
-            const cars = await carService.getAllCars(req.query);
-            res.json(cars);
-        } catch (error) {
-            res.status(500).json({ error: "An error occurred while retrieving the data." });
-        }
+  async getAllCars(req: Request, res: Response) {
+    try {
+      const cars = await carService.getAllCars(req.query);
+      res.status(HTTP_STATUS_CODES.OK).json(cars);
+    } catch (error) {
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ error: "An error occurred while retrieving the data." });
     }
+  },
+  async createCar(req: Request, res: Response) {
+    try {
+      const newProduct = await carService.createCar(req.body);
+      res.status(HTTP_STATUS_CODES.CREATED).json(newProduct);
+    } catch (err) {
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ error: "An error occurred while creating data." });
+    }
+  },
 };
 
 export default carController;
