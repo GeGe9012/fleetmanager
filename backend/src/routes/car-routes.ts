5b74d4632369f4dbd66a6335e114c8db32313448
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-21 15:34:37 +0100

    add yup validate

--- backend/src/routes/car-routes.ts
+++ backend/src/routes/car-routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import carController from "../controllers/car-controller";
+import yupValidate from "../middleware/validate";
+import { createCarSchema, updateCarSchema } from "../schemas/car-schema";
 
 const router = express.Router();
 
 router.get("/", carController.getAllCars);
-router.post("/", carController.createCar);
+router.post("/", yupValidate(createCarSchema), carController.createCar);
 router.delete("/:carId", carController.deleteCar);
-router.patch("/:carId", carController.updateCar);
+router.patch("/:carId", yupValidate(updateCarSchema), carController.updateCar);
 
 export default router;
