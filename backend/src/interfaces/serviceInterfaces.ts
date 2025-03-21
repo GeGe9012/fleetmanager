0a1301428df48f603b98af061e3f2bf17c013fc9
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-21 15:56:57 +0100

    create serviceInterfaces backend

---
+++ backend/src/interfaces/serviceInterfaces.ts
@@ -0,0 +1,35 @@
+export interface NewCarData {
+  license_plate: string;
+  make: string;
+  model: string;
+  model_year: number;
+  color: string;
+  fuel_type: string;
+  vin: string;
+  reg_date: string;
+  drivetrain: string;
+  warranty: string;
+}
+
+export interface NewCustomerData {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  email: string;
+  customer_address_1: string;
+  customer_address_2: string;
+  customer_address_3: string;
+  customer_address_4: string;
+  customer_tax_number: string;
+}
+
+export interface NewCompanyData {
+  company: string;
+  company_tax_number: string;
+  contact_phone_number: string;
+  reg_number: string;
+  company_address_1: string;
+  company_address_2: string;
+  company_address_3: string;
+  company_address_4: string;
+}
