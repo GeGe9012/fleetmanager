8c6cbbbab9dc026ade41e9e933f9bb2eef1c87de
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-21 14:37:17 +0100

    add update car route

--- backend/src/routes/car-routes.ts
+++ backend/src/routes/car-routes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/", carController.getAllCars);
 router.post("/", carController.createCar);
-router.delete("/:carId", carController.deleteCar)
+router.delete("/:carId", carController.deleteCar);
+router.patch("/:carId", carController.updateCar);
 
 export default router;
