c250c30c4ff726821fba857cd839041db559f806
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-21 15:34:44 +0100

    add yup validate

--- backend/src/routes/customer-routes.ts
+++ backend/src/routes/customer-routes.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import customerController from "../controllers/customer-controller";
+import yupValidate from "../middleware/validate";
+import {
+  createCustomerSchema,
+  updateCustomerSchema,
+} from "../schemas/customer-schema";
 
 const router = express.Router();
 
 router.get("/", customerController.getAllCustomers);
-router.post("/", customerController.createCustomer);
+router.post(
+  "/",
+  yupValidate(createCustomerSchema),
+  customerController.createCustomer
+);
 router.delete("/:customerId", customerController.deleteCustomer);
-router.patch("/:customerId", customerController.updateCustomer);
+router.patch(
+  "/:customerId",
+  yupValidate(updateCustomerSchema),
+  customerController.updateCustomer
+);
 
 export default router;
