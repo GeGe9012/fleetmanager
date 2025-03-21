4d76363ecccf4bdf7e55faf36779fb1aa151b1e5
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-21 15:53:55 +0100

    create interfaces

---
+++ frontend/src/interfaces/appInterfaces.ts
@@ -0,0 +1,42 @@
+export interface Car {
+  id: string;
+  license_plate: string;
+  make: string;
+  model: string;
+  model_year: number;
+  color: string;
+  fuel_type: boolean;
+  vin: string;
+  reg_date: number;
+  drivetrain: boolean;
+  warranty: string;
+  [key: string]: string | number | boolean;
+}
+
+export interface Customer {
+  id: string;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  email: string;
+  customer_address_1: string;
+  customer_address_2: string;
+  customer_address_3: string;
+  customer_address_4: string;
+  contract: string;
+  customer_tax_number: string;
+  [key: string]: string;
+}
+
+export interface Company {
+  id: string;
+  company: string;
+  company_tax_number: string;
+  contact_phone_number: string;
+  reg_number: string;
+  company_address_1: string;
+  company_address_2: string;
+  company_address_3: string;
+  company_address_4: string;
+  [key: string]: string;
+}
