6874c69568eecd346f68e0b439832e5ef233ce7b
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-21 13:34:45 +0100

    add delete customer route

--- backend/src/routes/customer-routes.ts
+++ backend/src/routes/customer-routes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/", customerController.getAllCustomers);
 router.post("/", customerController.createCustomer);
+router.delete("/:customerId", customerController.deleteCustomer);
 
 export default router;
