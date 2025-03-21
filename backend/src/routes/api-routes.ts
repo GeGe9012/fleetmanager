4eb9cabccbd935482443f1eb9ab215c3838a8dd9
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-21 14:35:48 +0100

    prettier

--- backend/src/routes/api-routes.ts
+++ backend/src/routes/api-routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import carRoutes from "./car-routes";
-import customerRoutes from "./customer-routes"
-import companyRoutes from "./company-routes"
+import customerRoutes from "./customer-routes";
+import companyRoutes from "./company-routes";
 
 const router = express.Router();
 
