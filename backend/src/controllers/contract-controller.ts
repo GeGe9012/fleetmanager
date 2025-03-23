817f9afca44a981b7dde28ccd35e1786079a80fe
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-23 16:08:25 +0100

    create contract controller

---
+++ backend/src/controllers/contract-controller.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import customerService from "../services/customer-service";
+import { HTTP_STATUS_CODES } from "../constants/http-status-codes";
+import contractService from "../services/contract-service";
+
+const contractController = {
+  async createContract(req: Request, res: Response, next: NextFunction) {
+    console.log(req.body)
+    try {
+      const newContract = await contractService.createContract(req.body);
+      res.status(HTTP_STATUS_CODES.CREATED).json(newContract);
+    } catch (err) {
+      next(err);
+    }
+  },
+  
+};
+
+export default contractController;
