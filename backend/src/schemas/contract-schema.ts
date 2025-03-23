a69d655b4105aa31522e68f6adf96ef660eb7ab1
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-23 16:07:52 +0100

    create new contract schema

---
+++ backend/src/schemas/contract-schema.ts
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+
+const createContractSchema = Yup.object().shape({
+  license_plate: Yup.string().required("Please select a car!"),
+  company_name: Yup.string().required("Please select a company!"),
+  customer_name: Yup.string().required("Please select a customer!"),
+  contract_exp: Yup.date()
+    .required("The registration date is required!")
+    .typeError("Please enter a valid date!"),
+});
+
+export default createContractSchema;
