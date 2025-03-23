eaaf98ffeda9548989ace0bff675656897f1042c
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-23 16:09:00 +0100

    integrate contract route

--- backend/src/routes/api-routes.ts
+++ backend/src/routes/api-routes.ts
@@ -2,11 +2,13 @@ import express from "express";
 import carRoutes from "./car-routes";
 import customerRoutes from "./customer-routes";
 import companyRoutes from "./company-routes";
+import contractRoutes from "./contract-routes";
 
 const router = express.Router();
 
 router.use("/cars", carRoutes);
 router.use("/customers", customerRoutes);
 router.use("/companies", companyRoutes);
+router.use("/contracts", contractRoutes);
 
 export default router;
