c5cfe91bee9f72fbacc42a7eb7d698b2f278f618
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-26 13:15:44 +0100

    integrate routes

--- backend/src/routes/api-routes.ts
+++ backend/src/routes/api-routes.ts
@@ -3,6 +3,9 @@ import carRoutes from "./car-routes";
 import customerRoutes from "./customer-routes";
 import companyRoutes from "./company-routes";
 import contractRoutes from "./contract-routes";
+import colorRoutes from "./color-routes";
+import makeRoutes from "./make-routes";
+import warrantyRoutes from "./warranty-routes";
 
 const router = express.Router();
 
@@ -10,5 +13,8 @@ router.use("/cars", carRoutes);
 router.use("/customers", customerRoutes);
 router.use("/companies", companyRoutes);
 router.use("/contracts", contractRoutes);
+router.use("/colors", colorRoutes);
+router.use("/makes", makeRoutes);
+router.use("/warranties", warrantyRoutes);
 
 export default router;
