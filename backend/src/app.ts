1ca7cdc79c66f0369e0c78c487d731a01e9e6038
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-27 14:15:21 +0100

    delete comments

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,18 +2,11 @@ import express from "express";
 import cors from "cors";
 import apiRoutes from "./routes/api-routes";
 import errorHandler from "./middleware/error-handler";
-// import swaggerUi from "swagger-ui-express";
-// import swaggerJSDoc from "swagger-jsdoc";
-// import swaggerOptions from "./constants/swagger-options";
 
 const app = express();
 
-// const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
 app.use(cors());
 app.use(express.json());
-
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Swagger UI integráció
 
 app.use("/api", apiRoutes);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,8 @@
 import http from "http";
 import app from "./app";
 import { PORT } from "./constants/base";
-// import { initializeSocket } from "./io.js";
 
 const server = http.createServer(app);
-
-// initializeSocket(server);
 
 server.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}...`);
