252f6940dba41703e5e747048ffbbb31878b328d
GeGe9012 <gergo.molnar90@gmail.com>
2025-03-30 16:40:13 +0200

    integrate delete and get contract

--- backend/src/controllers/contract-controller.ts
+++ backend/src/controllers/contract-controller.ts
@@ -1,13 +1,34 @@
 import { NextFunction, Request, Response } from "express";
-import customerService from "../services/customer-service";
 import { HTTP_STATUS_CODES } from "../constants/http-status-codes";
 import contractService from "../services/contract-service";
 
 const contractController = {
+  async getAllContracts(req: Request, res: Response, next: NextFunction) {
+    try {
+      const contracts = await contractService.getAllContracts(req.query);
+      res.status(HTTP_STATUS_CODES.OK).json(contracts);
+    } catch (err) {
+      next(err);
+    }
+  },
   async createContract(req: Request, res: Response, next: NextFunction) {
     try {
       const newContract = await contractService.createContract(req.body);
       res.status(HTTP_STATUS_CODES.CREATED).json(newContract);
+    } catch (err) {
+      next(err);
+    }
+  },
+  async deleteContract(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { contractId } = req.params;
+
+      const deletedContract = await contractService.deleteContract(contractId);
+      res.status(HTTP_STATUS_CODES.OK).json(deletedContract);
     } catch (err) {
       next(err);
     }
