1f5cecc39d1d31e9719865ba988fbac109b348a6
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-01 15:17:08 +0200

    bugfixing

--- backend/src/constants/base.ts
+++ backend/src/constants/base.ts
@@ -1,2 +1,2 @@
 export const DATABASE_URL = process.env.DATABASE_URL;
-export const PORT = process.env.PORT || 1000;
+export const PORT = process.env.PORT || 8080;
--- backend/src/websocket.ts
+++ backend/src/websocket.ts
@@ -1,12 +1,19 @@
 import { WebSocketServer } from "ws";
+import { Server } from "http";
 import resetDbService from "./services/resetdb-service";
 
-export function setupWebSocket(server: any) {
-  const wss = new WebSocketServer({ server });
-  
+export function setupWebSocket(server: Server) {
+  const wss = new WebSocketServer({ noServer: true });
+
+  server.on("upgrade", (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit("connection", ws, request);
+    });
+  });
+
   wss.on("connection", (ws) => {
     console.log("🔗 Client connected to WebSocket");
-    
+
     ws.on("close", () => {
       console.log("❌ Client disconnected");
       console.log("🛠️ Database reset triggered!");
