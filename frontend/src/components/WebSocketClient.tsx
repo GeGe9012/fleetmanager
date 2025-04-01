afb682ca958e5bf1ae08e529780324d95fe4d7d1
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-01 14:54:14 +0200

    bugfix

--- frontend/src/components/WebSocketClient.tsx
+++ frontend/src/components/WebSocketClient.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
-import { BACKEND_URL } from "../constants/backend";
+import { WSS_BACKEND_URL } from "../constants/backend";
 
 export default function WebSocketClient() {
   useEffect(() => {
-    const ws = new WebSocket(BACKEND_URL);
+    const ws = new WebSocket(WSS_BACKEND_URL);
 
     ws.onopen = () => console.log("✅ Connected to WebSocket");
     ws.onclose = () => console.log("❌ WebSocket connection closed");
--- frontend/src/constants/backend.ts
+++ frontend/src/constants/backend.ts
@@ -1,1 +1,2 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const WSS_BACKEND_URL = import.meta.env.VITE_WSS_BACKEND_URL;
