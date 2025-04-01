0f376bdd7fc99b69d3f40769338f0cf888e9b995
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-01 14:10:43 +0200

    exchange href to link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,12 +2,13 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="/" className="fs-1">
+        <Navbar.Brand as={Link} to="/" className="fs-1">
           <img
             src="./sport-car.png"
             alt="TFM logo"
@@ -23,7 +24,8 @@ export default function Header() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto d-flex align-items-center">
             <Nav.Link
-              href="/contracttool"
+              as={Link}
+              to="/contracttool"
               style={{
                 backgroundColor: "#007bff",
                 color: "white",
@@ -40,23 +42,21 @@ export default function Header() {
               Contracts
             </Nav.Link>
 
-            <NavDropdown
-              title="Tools"
-              id="basic-nav-dropdown"
-              className="custom-dropdown ms-3"
-            >
-              <NavDropdown.Item href="/newcar">Add new car</NavDropdown.Item>
-              <NavDropdown.Item href="/newcustomer">
+            <NavDropdown title="Tools" id="basic-nav-dropdown" className="custom-dropdown ms-3">
+              <NavDropdown.Item as={Link} to="/newcar">
+                Add new car
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/newcustomer">
                 Add new customer
               </NavDropdown.Item>
-              <NavDropdown.Item href="/newcompany">
+              <NavDropdown.Item as={Link} to="/newcompany">
                 Add new company
               </NavDropdown.Item>
-              <NavDropdown.Item href="/othertools">
+              <NavDropdown.Item as={Link} to="/othertools">
                 Other tools
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="https://aboutme-mg.vercel.app/">
+              <NavDropdown.Item as="a" href="https://aboutme-mg.vercel.app/" target="_blank" rel="noopener noreferrer">
                 About me
               </NavDropdown.Item>
             </NavDropdown>
@@ -77,7 +77,6 @@ export default function Header() {
           .dropdown-menu .dropdown-item {
             transition: background-color 0.3s ease, color 0.3s ease;
           }
-          
           .custom-dropdown .dropdown-menu .dropdown-item:hover {
             background-color: #007bff !important;
             color: white !important;
