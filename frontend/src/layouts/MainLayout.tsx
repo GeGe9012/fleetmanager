cf7ebf06303882ce9a5762596a01d77bd4d6b901
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-01 14:11:20 +0200

    add reset db feature

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -2,9 +2,20 @@ import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Header from "../components/Header";
 import { Stack } from "react-bootstrap";
+import { resetDatabase } from "../services/resetDbService";
 
 export default function MainLayout() {
   const location = useLocation();
+
+  useEffect(() => {
+    const handleUnload = () => {
+      resetDatabase();
+    };
+    window.addEventListener("beforeunload", handleUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+    };
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
