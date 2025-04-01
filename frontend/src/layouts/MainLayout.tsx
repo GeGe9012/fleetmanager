e3827b48168a5d14096693f82a88545afeb3d8d4
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-01 14:39:52 +0200

    integrate websocket

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -2,20 +2,10 @@ import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Header from "../components/Header";
 import { Stack } from "react-bootstrap";
-import { resetDatabase } from "../services/resetDbService";
+import WebSocketClient from "../components/WebSocketClient";
 
 export default function MainLayout() {
   const location = useLocation();
-
-  useEffect(() => {
-    const handleUnload = () => {
-      resetDatabase();
-    };
-    window.addEventListener("beforeunload", handleUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleUnload);
-    };
-  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -25,6 +15,7 @@ export default function MainLayout() {
     <Stack gap={4}>
       <Header />
       <Outlet />
+      <WebSocketClient />
     </Stack>
   );
 }
