816cf9ed92bca68a307a36661e45e72362de7c37
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-02 15:08:59 +0200

    remove websocket server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,8 @@
 import http from "http";
 import app from "./app";
 import { PORT } from "./constants/base";
-import { setupWebSocket } from "./websocket";
 
 const server = http.createServer(app);
-
-setupWebSocket(server);
 
 server.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}...`);
