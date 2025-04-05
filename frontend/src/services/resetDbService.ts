b1c9c02d168938d63af88545c21b47a5b37f4ca1
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-05 11:48:58 +0200

    bugfix

--- frontend/src/services/resetDbService.ts
+++ frontend/src/services/resetDbService.ts
@@ -3,7 +3,8 @@ import { BACKEND_URL } from "../constants/backend";
 
 export async function resetDatabase() {
   try {
-    await axios.post(`${BACKEND_URL}/api/resetdb`);
+    const response = await axios.post(`${BACKEND_URL}/api/resetdb`);
+    return response.data;
   } catch (error) {
     console.error("Failed to send reset database request.", error);
     throw new Error("Failed to reset database.");
