b35fa7e9898570d09938e2a6b703db7d5d1066c5
GeGe9012 <gergo.molnar90@gmail.com>
2025-04-05 12:44:12 +0200

    test

--- backend/src/services/resetdb-service.ts
+++ backend/src/services/resetdb-service.ts
@@ -13,45 +13,15 @@ import { Prisma } from "@prisma/client";
 const resetDbService = {
   async resetDatabase() {
     try {
-      // Törlés batch-ben
       await prisma.warranty.deleteMany();
-      await prisma.color.deleteMany();
-      await prisma.make.deleteMany();
-      await prisma.car.deleteMany();
-      await prisma.company.deleteMany();  // Törlés először
-      await prisma.customer.deleteMany();
-      await prisma.contract.deleteMany();
-
-      // Létrehozás batch-ben
-      await this.createBatch(warranties, prisma.warranty);
-      await this.createBatch(colors, prisma.color);
-      await this.createBatch(makes, prisma.make);
-      await this.createBatch(cars, prisma.car);
-
-      // Először a company-ket hozom létre
-      await this.createBatch(companies, prisma.company);
-
-      // Majd a contract-ok létrehozása a companies-re hivatkozva
-      await this.createBatch(customers, prisma.customer);
-
-      // Végül a contract-ok létrehozása
-      await this.createBatch(contracts, prisma.contract);
-
+      await prisma.warranty.createMany({ data: warranties });
     } catch (error) {
-      console.log(error)
-      console.error("Hiba történt az adatbázis resetelésekor:", error);
-      throw new HttpError("Failed to reset database.", HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR);
-    }
-  },
-
-  async createBatch(data: any[], model: any) {
-    const batchSize = 1000;  // Mennyiség, amit egyszerre küldesz
-    for (let i = 0; i < data.length; i += batchSize) {
-      const batch = data.slice(i, i + batchSize);
-      await model.createMany({ data: batch });
+      throw new HttpError(
+        "Failed to reset database.",
+        HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
+      );
     }
   },
 };
 
 export default resetDbService;
-
